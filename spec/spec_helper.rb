616528c076151170f189c2b9c342d4b0bb4dd9e9
Ryan Bigg <me@ryanbigg.com>
2021-09-28 19:26:41 +1000

    Add GameDSL for quick game setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 require "pry"
 require "magic"
+require_relative "support/game_dsl"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -12,4 +13,6 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  config.include GameDSL
 end
