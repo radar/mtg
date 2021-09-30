d640e8809b6073fcb13553d6b845164850bd3ed2
Ryan Bigg <me@ryanbigg.com>
2021-09-30 21:15:37 +1000

    Split mana spend tests

---
+++ spec/game/integration/mana/essence_warden_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+RSpec.describe Magic::Game, "Mana spend -- Essence Warden" do
+  subject(:game) { Magic::Game.new }
+
+  let(:p1) { game.add_player }
+  let(:p2) { game.add_player }
+
+  context "when at first main phase" do
+    before do
+      subject.step.force_state!(:draw)
+      subject.next_step
+    end
+
+    context "when mana cost is not payable" do
+      let(:essence_warden) { Card("Essence Warden", game: game) }
+      before do
+        p1.hand.add(essence_warden)
+      end
+
+      it "cannot cast the essence warden" do
+        expect(p1.can_cast?(essence_warden)).to eq(false)
+      end
+    end
+
+    context "with a forest on the battlefield, and essence warden in hand" do
+      let(:essence_warden) { Card("Essence Warden", game: game) }
+      let(:forest) { Card("Forest", game: game) }
+
+      before do
+        p1.hand.add(forest)
+        p1.hand.add(essence_warden)
+      end
+
+      it "casts a forest, then an essence warden" do
+        expect(p1.can_cast?(forest)).to eq(true)
+        p1.cast!(forest)
+        p1.tap!(forest)
+        expect(p1.mana_pool[:green]).to eq(1)
+        expect(p1.can_cast?(essence_warden)).to eq(true)
+        p1.pay_and_cast!({ green: 1 }, essence_warden)
+        game.stack.resolve!
+        expect(essence_warden.zone).to be_battlefield
+        expect(p1.mana_pool[:green]).to eq(0)
+      end
+    end
+  end
+end
