38273deff479042947adf56a85b4cf2fa5170ba2
Ryan Bigg <me@ryanbigg.com>
2022-04-26 14:34:33 +1000

    Refactor card_list select

--- lib/magic/card_list.rb
+++ lib/magic/card_list.rb
@@ -1,44 +1,55 @@
 module Magic
   class CardList < SimpleDelegator
     def controlled_by(player)
-      self.class.new(select { |card| card.controller == player })
+      select { |card| card.controller == player }
     end
 
     def not_controlled_by(player)
-      self.class.new(select { |card| card.controller != player })
+      select { |card| card.controller != player }
+    end
+
+    def basic_lands
+      select(&:basic_land?)
     end
 
     def creatures
-      self.class.new(select(&:creature?))
+      select(&:creature?)
     end
 
     def planeswalkers
-      self.class.new(select(&:planeswalker?))
+      select(&:planeswalker?)
     end
 
     def permanents
-      self.class.new(select(&:permanent?))
+      select(&:permanent?)
     end
 
     def dead
-      self.class.new(select(&:dead?))
+      select(&:dead?)
     end
 
     def with_power(&block)
-      self.class.new(select { |creature| yield(creature.power) })
+      select { |creature| yield(creature.power) }
     end
 
     def by_name(name)
-      self.class.new(select { |c| c.name == name })
+      select { |c| c.name == name }
     end
 
     def by_any_type(*types)
-      self.class.new(select { |c| types.any? { |type| c.type?(type) } })
+      select { |c| types.any? { |type| c.type?(type) } }
     end
     alias_method :by_type, :by_any_type
 
     def except(target)
       self.class.new(reject { |c| c == target })
     end
+
+    private
+
+    def select(&condition)
+      self.class.new(super(&condition))
+    end
+
   end
 end
