9abcf92304e17d91f01e71cddb3f0e5e67a50ba9
Ryan Bigg <me@ryanbigg.com>
2023-02-16 09:48:02 +1100

    Add Valorous Steed

---
+++ spec/cards/valorous_steed_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+RSpec.describe Magic::Cards::ValorousSteed do
+  include_context "two player game"
+
+  subject(:valorous_steed) { Permanent("Valorous Steed", owner: p1) }
+
+  before do
+    game.battlefield.add(subject)
+  end
+
+  it "has vigilance" do
+    expect(subject).to be_vigilant
+  end
+
+  context "when it enters the battlefield" do
+    it "creates a 2/2 white Knight with vigilance" do
+      subject.entered_the_battlefield!
+      knight = game.battlefield.creatures.by_name("Knight").controlled_by(p1).first
+      expect(knight).not_to be_nil
+      expect(knight).to be_vigilant
+    end
+  end
+end
