766bf50a042e25431fd6ff00a8db3e9bb6354acd
Josh Carvell <j_carvell@hotmail.co.uk>
2023-03-08 19:24:54 +0000

    Add Sure Strike Test

--- lib/magic/cards/sure_strike.rb
+++ lib/magic/cards/sure_strike.rb
@@ -2,23 +2,18 @@ module Magic
   module Cards
     SureStrike = Instant("Sure Strike") do
       cost generic: 1, red: 1
-    end
-
-    class SureStrike < Instant
-      def target_choices
-        battlefield.creatures
-      end
 
       def single_target?
         true
       end
 
-      def resolve!(controller, target:)
-          game.add_effect(Effects::ApplyBuff.new(source: self, power: 3, targets: target))
-          target.grant_keyword(Keywords::FIRST_STRIKE, until_eot: true)
-        end
+      def target_choices
+        battlefield.creatures
+      end
 
-        super
+      def resolve!(controller, target:)
+        game.add_effect(Effects::ApplyBuff.new(source: self, power: 3, targets: target))
+        target.grant_keyword(Keywords::FIRST_STRIKE, until_eot: true) 
       end
     end
   end
---
+++ spec/cards/sure_strike_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+RSpec.describe Magic::Cards::SureStrike do
+  include_context "two player game"
+
+  let!(:sure_strike) { described_class.new(game: game) }
+  let!(:onakke_ogre) { ResolvePermanent("Onakke Ogre", owner: p1) }
+  context "with a creature in play" do
+    
+
+    it "grants first strike and 3 power" do
+      p1.add_mana(red: 2)
+      action = Magic::Actions::Cast.new(player: p1, card: sure_strike)
+        .pay_mana(generic: { red: 1 }, red: 1)
+        .targeting(onakke_ogre)
+      game.take_action(action)
+      game.tick!
+
+      expect(onakke_ogre.power).to eq(7)
+      expect(onakke_ogre.toughness).to eq(2)
+
+      game.current_turn.end!
+      game.current_turn.cleanup!
+      game.next_turn
+
+      expect(onakke_ogre.power).to eq(4)
+      expect(onakke_ogre.first_strike?).to eq(false)
+
+    end
+
+  end
+end
