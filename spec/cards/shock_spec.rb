c16a25f7d7e8faa78890ea9df4507d4a12f3cd93
Josh Carvell <j_carvell@hotmail.co.uk>
2023-03-08 19:42:08 +0000

    Add Shock tests for targeting create and player

---
+++ spec/cards/shock_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+RSpec.describe Magic::Cards::Shock do
+  include_context "two player game"
+
+  let!(:shock) { described_class.new(game: game) }
+  let!(:cloudkin_seer) { ResolvePermanent("Cloudkin Seer", owner: p2) }
+
+  it "destroys the Jeskai Elder" do
+    p1.add_mana(red: 1)
+    action = Magic::Actions::Cast.new(player: p1, card: shock)
+        .pay_mana(red: 1)
+        .targeting(cloudkin_seer)
+    game.take_action(action)
+    game.tick!
+    expect(cloudkin_seer).to be_dead
+  end
+
+  it "targets the opponent" do
+    p2_life_total = p2.life
+    p1.add_mana(red: 1)
+    action = Magic::Actions::Cast.new(player: p1, card: shock)
+        .pay_mana(red: 1)
+        .targeting(p2)
+    game.take_action(action)
+    game.tick!
+    expect(p2.life).to eq(p2_life_total - 2)
+  end
+end
