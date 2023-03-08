ffdc598213b5f44f31b8b35bd28659e9f903ed25
Josh Carvell <j_carvell@hotmail.co.uk>
2023-03-08 18:28:05 +0000

    Add Sure Strike

---
+++ lib/magic/cards/sure_strike.rb
@@ -0,0 +1,25 @@
+module Magic
+  module Cards
+    SureStrike = Instant("Sure Strike") do
+      cost white: 1
+    end
+
+    class SureStrike < Instant
+      def target_choices
+        battlefield.creatures
+      end
+
+      def single_target?
+        true
+      end
+
+      def resolve!(controller, target:)
+          game.add_effect(Effects::ApplyBuff.new(source: self, power: 3, targets: target))
+          target.grant_keyword(Keywords::FIRST_STRIKE, until_eot: true)
+        end
+
+        super
+      end
+    end
+  end
+end
