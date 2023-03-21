537b34151df30f754506b4d561c8c5ec15e7c6ff
Josh Carvell <j_carvell@hotmail.co.uk>
2023-03-21 21:43:56 +0000

    Add Burn Bright Test

--- spec/cards/burn_bright_spec.rb
+++ spec/cards/burn_bright_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe Magic::Cards::BurnBright do
   include_context "two player game"
 
   let!(:burn_bright) { described_class.new(game: game) }
-  let!(:blood_glutton) { ResolvePermanent("Blood Glutton", owner: p1) }
-  let!(:onakke_ogre) { ResolvePermanent("Onakke Ogre", owner: p1) }
 
   context "with 2 creature in play" do
-    
+
+    let!(:blood_glutton) { ResolvePermanent("Blood Glutton", owner: p1) }
+    let!(:onakke_ogre) { ResolvePermanent("Onakke Ogre", owner: p1) }
+
     it "grants 2 power to both creatures " do
-      expect(onakke_ogre.power).to eq(4)
       p1.add_mana(red: 3)
       action = Magic::Actions::Cast.new(player: p1, card: burn_bright)
         .pay_mana(generic: { red: 2 }, red: 1)
@@ -19,15 +19,32 @@ RSpec.describe Magic::Cards::BurnBright do
 
       expect(onakke_ogre.power).to eq(6)
       expect(onakke_ogre.toughness).to eq(2)
+      expect(blood_glutton.power).to eq(6)
+      expect(blood_glutton.toughness).to eq(3)
 
-      # game.current_turn.end!
-      # game.current_turn.cleanup!
-      # game.next_turn
+      game.current_turn.end!
+      game.current_turn.cleanup!
+      game.next_turn
 
-      # expect(onakke_ogre.power).to eq(4)
-      # expect(onakke_ogre.first_strike?).to eq(false)
-
+      expect(onakke_ogre.power).to eq(4)
+      expect(blood_glutton.power).to eq(4)
     end
+  end
 
+  context "with 1 opposing creature in play" do
+
+    let!(:onakke_ogre) { ResolvePermanent("Onakke Ogre", owner: p1) }
+    let!(:cloudkin_seer) { ResolvePermanent("Cloudkin Seer", owner: p2) }
+
+    it "does not buff opponent's creature " do
+      p1.add_mana(red: 3)
+      action = Magic::Actions::Cast.new(player: p1, card: burn_bright)
+        .pay_mana(generic: { red: 2 }, red: 1)
+      game.take_action(action)
+      game.tick!
+
+      expect(cloudkin_seer.power).to eq(2)
+      expect(onakke_ogre.power).to eq(6)
+    end
   end
 end
