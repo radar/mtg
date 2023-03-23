d89fc2cd3f8d8f73ba2f8d4bf9a5489222f989ed
Josh Carvell <j_carvell@hotmail.co.uk>
2023-03-23 18:49:09 +0000

    Add Goblin Wizardry tests for prowess triggering correctly

--- spec/cards/goblin_wizardry_spec.rb
+++ spec/cards/goblin_wizardry_spec.rb
@@ -5,48 +5,55 @@ RSpec.describe Magic::Cards::GoblinWizardry do
 
   subject(:goblin_wizardry) { described_class.new(game: game) }
 
-  context "Empty Board" do
-
-    before do 
+  context "casting Goblin Wizardry" do
+    before do
       p1.add_mana(red: 4)
       action = Magic::Actions::Cast.new(player: p1, card: goblin_wizardry)
         .pay_mana(generic: { red: 3 }, red: 1)
       game.take_action(action)
       game.tick!
-    end 
-   
-    it "summons two goblin wizard tokens for p1" do
-      expect(game.battlefield.creatures.controlled_by(p1).length).to eq(2)
-      expect(game.battlefield.creatures.controlled_by(p2).length).to eq(0)
-      first_creature = game.battlefield.creatures.controlled_by(p1).first
-      second_creature = game.battlefield.creatures.controlled_by(p1).last
-      expect(first_creature.name).to eq("Goblin Wizard")
-      expect(first_creature.power).to eq(1)
-      expect(first_creature.toughness).to eq(1)
-      expect(first_creature.colors).to eq([:red])
-      expect(second_creature.name).to eq("Goblin Wizard")
-      expect(second_creature.power).to eq(1)
-      expect(second_creature.toughness).to eq(1)
-
     end
 
-    # it "buffs the tokens when a noncreature spell is cast " do
-    #   p1.add_mana(white: 1, blue: 1)
-    #   action = cast_action(player: p1, card: Card("Revitalize"))
-    #     .pay_mana(white: 1, generic: { blue: 1 })
-    #     .perform
-    #   game.tick!
-    #   first_creature = game.battlefield.creatures.controlled_by(p1).first
-    #   second_creature = game.battlefield.creatures.controlled_by(p1).last
-    #   expect(first_creature.name).to eq("Goblin Wizard")
-    #   expect(second_creature.name).to eq("Goblin Wizard")
-    #   expect(first_creature.power).to eq(2)
-    #   expect(first_creature.toughness).to eq(2)
-      
+    def goblin_wizards
+      game.battlefield.creatures
+        .controlled_by(p1)
+        .select { |creature| creature.name == "Goblin Wizard" }
+    end
 
-    # end
+    it "summons two Goblin Wizard tokens for p1" do
+      expect(goblin_wizards.length).to eq(2)
+      expect(goblin_wizards).to all(have_attributes(
+        name: "Goblin Wizard",
+        power: 1,
+        toughness: 1,
+        colors: [:red],
+      ))
+    end
 
+    it "buffs the tokens when a noncreature spell is cast" do
+      p1.add_mana(white: 1, blue: 1)
+      cast_action = cast_action(player: p1, card: Card("Revitalize"))
+        .pay_mana(white: 1, generic: { blue: 1 })
+        .perform
+      game.tick!
+      expect(goblin_wizards).to all(have_attributes(
+        name: "Goblin Wizard",
+        power: 2,
+        toughness: 2,
+      ))
+    end
 
+    it "has regular power and toughness when a creature is cast" do
+      p1.add_mana(red: 2, black: 1)
+      cast_action = cast_action(player: p1, card: Card("Onakke Ogre"))
+        .pay_mana(red: 1, generic: { red: 1, black: 1 })
+        .perform
+      game.tick!
+      expect(goblin_wizards).to all(have_attributes(
+        name: "Goblin Wizard",
+        power: 1,
+        toughness: 1,
+      ))
+    end
   end
-
 end
