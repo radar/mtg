f77cdcc36270ef729d59e5a53caa6b77cf10288f
Ariel Fogel <fogeltine@gmail.com>
2023-03-26 06:45:51 +0300

    add in additional containment_priest functionality

--- lib/magic/cards/containment_priest.rb
+++ lib/magic/cards/containment_priest.rb
@@ -6,6 +6,16 @@ module Magic
       keywords :flash
       power 2
       toughness 2
+
+      def event_handlers
+        {
+          Events::EnteredTheBattlefield => -> (receiver, event) do
+            return if receiver == event.permanent
+            return if game.current_turn.events[-3].is_a? Magic::Events::SpellCast
+            event.permanent.exile! unless event.permanent.token?
+          end
+        }
+      end
     end
   end
 end
--- spec/cards/containment_priest_spec.rb
+++ spec/cards/containment_priest_spec.rb
@@ -4,13 +4,12 @@ RSpec.describe Magic::Cards::ContainmentPriest do
   include_context "two player game"
 
   subject! { ResolvePermanent("Containment Priest", owner: p1) }
-  # Containment Priest
-  # # Capture Sphere {1}{U}{1}{W}
+  # Containment Priest {1}{U}{1}{W}
   # Creature -- Human Cleric
   # Flash (You may cast this spell any time you could cast an instant.)
-  # Enchant creature
+  # If a nontoken creature would enter the battlefield and it wasn’t cast, exile it instead.
 
-  context "base card atrributes" do
+  context "card attributes" do
     it "is a human cleric" do
       expect(subject.card.type_line).to eq("Creature -- Human Cleric")
     end
@@ -22,6 +21,42 @@ RSpec.describe Magic::Cards::ContainmentPriest do
     it "has a power + toughness" do
       expect(subject.power).to eq(2)
       expect(subject.toughness).to eq(2)
+    end
+  end
+
+  context "when a non-token creature enters the battlefield" do
+    context "the creature is not cast" do
+      it "exiles the creature" do
+        expect{
+          ResolvePermanent('Story Seeker', owner: p2)
+        }.to change { game.exile.cards.count }.by(1)
+        expect(game.battlefield.permanents.count).to eq(1)
+        expect(game.battlefield.permanents.map(&:name)).to_not include('Story Seeker')
+      end
+    end
+    context "the creature is cast" do
+      it "does not exile the creature" do
+        story_seeker = Card('Story Seeker')
+        p2.hand.add(story_seeker)
+        story_seeker.zone = p2.hand
+
+        p2.add_mana(white: 2)
+        action = Magic::Actions::Cast.new(player: p2, card: story_seeker)
+        action.pay_mana(generic: { white: 1 }, white: 1)
+        game.take_action(action)
+        game.tick!
+
+        expect(game.battlefield.permanents.count).to eq(2)
+        expect(game.battlefield.permanents.map(&:name)).to include('Story Seeker')
+      end
+    end
+  end
+
+  context "when a token creature enters the battlefield" do
+    it "does not exile the creature" do
+      scute = ResolvePermanent("Scute Swarm", owner: p1, token: true)
+
+      expect(game.battlefield.permanents.count).to eq(2)
     end
   end
 end
