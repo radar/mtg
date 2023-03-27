dfb43ee99cdf3c68a0e37ed357d7d0c759a47125
Ryan Bigg <me@ryanbigg.com>
2023-03-27 20:37:34 +1100

    Add Scavenging Ooze
    Fixes #214

---
+++ lib/magic/cards/scavenging_ooze.rb
@@ -0,0 +1,32 @@
+module Magic
+  module Cards
+    ScavengingOoze = Creature("Scavenging Ooze") do
+      cost generic: 1, green: 1
+      creature_type "Ooze"
+    end
+
+    class ScavengingOoze < Creature
+      class ActivatedAbility < Magic::ActivatedAbility
+        def costs = [Costs::Mana.new(green: 1)]
+
+        def single_target?
+          true
+        end
+
+        def target_choices
+          game.players.flat_map { _1.graveyard.cards }
+        end
+
+        def resolve!(target:)
+          Effects::Exile.new(source: source).resolve(target: target)
+          if target.creature?
+            source.add_counter(Counters::Plus1Plus1)
+            source.controller.gain_life(1)
+          end
+        end
+      end
+
+      def activated_abilities = [ActivatedAbility]
+    end
+  end
+end
---
+++ spec/cards/scavenging_ooze_spec.rb
@@ -0,0 +1,66 @@
+require 'spec_helper'
+
+RSpec.describe Magic::Cards::ScavengingOoze do
+  include_context "two player game"
+
+  let(:scavenging_ooze) { ResolvePermanent("Scavenging Ooze", owner: p1) }
+
+  context "activated ability" do
+    def ability
+      scavenging_ooze.activated_abilities.first
+    end
+
+    context "when p2 has wood elves in their graveyard" do
+      let(:wood_elves) { Card("Wood Elves") }
+
+      before do
+        p2.graveyard << wood_elves
+      end
+
+      it "exiles a card from a graveyard, adds +1/1 counter and p1 gains life" do
+        p1.add_mana(green: 1)
+        starting_life = p1.life
+        action = Magic::Actions::ActivateAbility.new(player: p1, permanent: scavenging_ooze, ability: ability)
+        action.pay_mana(green: 1)
+        action.targeting(wood_elves)
+        game.take_action(action)
+        game.stack.resolve!
+
+        aggregate_failures do
+          expect(wood_elves.zone).to be_exile
+
+          expect(scavenging_ooze.counters.count).to eq(1)
+          expect(scavenging_ooze.counters.first).to be_a(Magic::Counters::Plus1Plus1)
+
+          expect(p1.life).to eq(starting_life + 1)
+        end
+      end
+    end
+
+    context "when p2 has sol ring in their graveyard" do
+      let(:sol_ring) { Card("Sol Ring") }
+
+      before do
+        p2.graveyard << sol_ring
+      end
+
+      it "exiles sol ring, does not add counter and p1 does not gain life" do
+        p1.add_mana(green: 1)
+        starting_life = p1.life
+        action = Magic::Actions::ActivateAbility.new(player: p1, permanent: scavenging_ooze, ability: ability)
+        action.pay_mana(green: 1)
+        action.targeting(sol_ring)
+        game.take_action(action)
+        game.stack.resolve!
+
+        aggregate_failures do
+          expect(sol_ring.zone).to be_exile
+
+          expect(scavenging_ooze.counters.count).to eq(0)
+
+          expect(p1.life).to eq(starting_life)
+        end
+      end
+    end
+  end
+end
