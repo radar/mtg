280b95cff38fdde8b98b04b6850173fd4e31f260
Ryan Bigg <me@ryanbigg.com>
2023-12-18 10:41:47 +1100

    Fix syntax issue

--- lib/magic/cards/woolly_thoctar.rb
+++ lib/magic/cards/woolly_thoctar.rb
@@ -1,7 +1,7 @@
 module Magic
   module Cards
     WoollyThoctar = Creature("Woolly Thoctar") do
-      cost red: 1, green: 1, white: 1,
+      cost red: 1, green: 1, white: 1
       creature_type("Beast")
       power 5
       toughness 4
