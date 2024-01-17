24183afe874078eea30c4fbff841994a3a5463ba
Ryan Bigg <me@ryanbigg.com>
2024-01-18 07:36:55 +1100

    Use etb DSL for Acidic Slime

--- lib/magic/cards/acidic_slime.rb
+++ lib/magic/cards/acidic_slime.rb
@@ -6,25 +6,20 @@ module Magic
       keywords :deathtouch
       power 2
       toughness 2
+
+      enters_the_battlefield do
+        effect = Effects::DestroyTarget.new(
+            source: permanent,
+          )
+        game.add_effect(effect)
+      end
+
     end
 
     class AcidicSlime < Creature
       def target_choices(_)
         game.battlefield.cards.by_any_type("Artifact", "Enchantment", "Land")
       end
-
-      class ETB < TriggeredAbility::EnterTheBattlefield
-        def perform
-
-          effect = Effects::DestroyTarget.new(
-            source: permanent,
-          )
-          game.add_effect(effect)
-        end
-      end
-
-      def etb_triggers = [ETB]
-
     end
   end
 end
