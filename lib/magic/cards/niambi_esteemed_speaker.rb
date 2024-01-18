b2204fc36a40495d9f0ed4536480690f9a201f07
Ryan Bigg <me@ryanbigg.com>
2024-01-18 18:07:49 +1100

    Add Niambi, Esteemed Speaker
    Closes #480

---
+++ lib/magic/cards/niambi_esteemed_speaker.rb
@@ -0,0 +1,46 @@
+module Magic
+  module Cards
+    NiambiEsteemedSpeaker = Creature("Niambi, Esteemed Speaker") do
+      type "Legendary Creature -- Human Cleric"
+      cost white: 1, blue: 1
+      power 2
+      toughness 1
+      keywords :flash
+
+      enters_the_battlefield do
+        valid_targets = game.battlefield.creatures.controlled_by(controller).except(permanent)
+        game.choices.add(NiambiEsteemedSpeaker::Choice.new(source: permanent, choices: valid_targets))
+      end
+    end
+
+    class NiambiEsteemedSpeaker < Creature
+      class Choice < Magic::Choice::Effect
+        attr_reader :source
+
+        def initialize(source:, choices:)
+          @source = source
+          @choices = choices
+        end
+
+        def optional? = true
+
+        def choose(target:)
+          target.return_to_hand
+          source.controller.gain_life(target.mana_value)
+        end
+      end
+
+      class ActivatedAbility < Magic::ActivatedAbility
+        def costs
+          [Costs::Mana.new(generic: 1, white: 1, blue: 1), Costs::Tap.new(source), Costs::Discard.new(source.controller, -> (c) { c.legendary? })]
+        end
+
+        def resolve!
+          2.times { source.controller.draw! }
+        end
+      end
+
+      def activated_abilities = [ActivatedAbility]
+    end
+  end
+end
---
+++ spec/cards/niambi_esteemed_speaker_spec.rb
@@ -0,0 +1,61 @@
+require 'spec_helper'
+
+RSpec.describe Magic::Cards::NiambiEsteemedSpeaker do
+  include_context "two player game"
+
+  let(:card) { Card("Niambi, Esteemed Speaker")}
+
+  before do
+    ResolvePermanent("Wood Elves", owner: p2)
+  end
+
+  it "has flash" do
+    expect(card.flash?).to eq(true)
+  end
+
+  context "when it enters the battlefield" do
+    it "chooses to return card to hand" do
+      ResolvePermanent("Niambi, Esteemed Speaker", owner: p1)
+
+      choice = game.choices.first
+      expect(choice).to be_a(Magic::Cards::NiambiEsteemedSpeaker::Choice)
+      choice.choose(target: game.battlefield.creatures.by_name("Wood Elves").first)
+
+      expect(p1.life).to eq(23)
+      expect(p2.hand.by_name("Wood Elves").count).to eq(1)
+    end
+
+
+    it "chooses to skip the choice" do
+      ResolvePermanent("Niambi, Esteemed Speaker", owner: p1)
+
+      choice = game.choices.first
+      expect(choice).to be_a(Magic::Cards::NiambiEsteemedSpeaker::Choice)
+      p1.skip_choice(choice)
+      expect(game.choices).to be_empty
+
+      expect(p1.life).to eq(20)
+      expect(game.battlefield.creatures.by_name("Wood Elves").count).to eq(1)
+    end
+  end
+
+  context "activated ability" do
+    let!(:niambi) { ResolvePermanent("Niambi, Esteemed Speaker", owner: p1) }
+    before do
+      p1.hand.add(Card("Aron, Benalia's Ruin"))
+    end
+
+    it "draws two cards" do
+      expect(p1).to receive(:draw!).twice
+
+      aron = p1.hand.by_name("Aron, Benalia's Ruin").first
+      p1.add_mana(white: 2, blue: 1)
+      p1.activate_ability(ability: niambi.activated_abilities.first) do
+        _1.pay_mana(white: 1, blue: 1, generic: { white: 1 })
+        _1.pay_discard(aron)
+      end
+
+      expect(aron.zone).to be_graveyard
+    end
+  end
+end
