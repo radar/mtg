9094614c3a997c887577a0123e27ef107ff12e31
Ryan Bigg <me@ryanbigg.com>
2024-02-04 22:12:45 +1100

    Add Watcher of the Spheres
    Fixes #227

---
+++ lib/magic/cards/watcher_of_the_spheres.rb
@@ -0,0 +1,37 @@
+module Magic
+  module Cards
+    class WatcherOfTheSpheres < Creature
+      name "Watcher of the Spheres"
+      creature_type "Bird Wizard"
+      cost white: 1, blue: 1
+      keywords :flying
+      power 2
+      toughness 2
+
+      class ReduceManaCost < Abilities::Static::ManaCostAdjustment
+        def initialize(source:)
+          @source = source
+          @adjustment = { generic: -1 }
+          @applies_to = -> (c) { c.flying? }
+        end
+      end
+
+      def static_abilities = [ReduceManaCost]
+
+      def event_handlers
+        {
+          Events::EnteredTheBattlefield => -> (receiver, event) do
+            return if event.permanent == receiver
+            return unless event.permanent.flying?
+            return unless event.permanent.controller == receiver.controller
+
+            receiver.modify_power(1)
+            receiver.modify_toughness(1)
+          end
+        }
+
+      end
+
+    end
+  end
+end
---
+++ spec/cards/watcher_of_the_spheres_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+RSpec.describe Magic::Cards::WatcherOfTheSpheres do
+  include_context "two player game"
+
+  let!(:watcher_of_the_spheres) { ResolvePermanent("Watcher Of The Spheres", owner: p1) }
+
+  it "permanent has a static ability" do
+    ability = watcher_of_the_spheres.static_abilities.first
+    expect(ability).to be_a(Magic::Abilities::Static::ManaCostAdjustment)
+  end
+
+  context "when another creature with flying enters, add a +1/+1 counter" do
+    before do
+      ResolvePermanent("Aven Gagglemaster", owner: p1)
+    end
+
+    it "adds a +1/+1 buff" do
+      expect(watcher_of_the_spheres.power).to eq(3)
+      expect(watcher_of_the_spheres.toughness).to eq(3)
+    end
+  end
+end
