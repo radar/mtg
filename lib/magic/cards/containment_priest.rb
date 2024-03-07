21447dffe90b06029118c8fbc9fde9affd1672a0
Ryan Bigg <me@ryanbigg.com>
2024-03-07 21:42:17 +1100

    Use new replacement effect syntax for Containment Priest

--- lib/magic/cards/containment_priest.rb
+++ lib/magic/cards/containment_priest.rb
@@ -1,6 +1,7 @@
 module Magic
   module Cards
-    ContainmentPriest = Creature("Containment Priest") do
+    class ContainmentPriest < Creature
+      card_name "Containment Priest"
       cost generic: 1, white: 1
       creature_type("Human Cleric")
       keywords :flash
@@ -9,18 +10,26 @@ module Magic
     end
 
     class ContainmentPriest < Creature
+      class ExileEffect < ReplacementEffect
+        def applies?(effect)
+          permanent = effect.permanent
+          effect.to.battlefield? &&
+            permanent.creature? &&
+            !permanent.token? &&
+            !permanent.cast?
+        end
+
+        def call(effect)
+          Effects::ExilePermanent.new(
+            source: receiver,
+            target: effect.permanent,
+          )
+        end
+      end
+
       def replacement_effects
         {
-          Effects::MovePermanentZone => -> (receiver, event) do
-            return unless event.to.battlefield?
-            return if receiver == event.permanent
-            return if event.permanent.cast? || event.permanent.token?
-
-            Effects::ExilePermanent.new(
-              source: receiver,
-              target: event.permanent,
-            )
-          end
+          Effects::MovePermanentZone => ExileEffect
         }
       end
     end
