5910734b32ddd99e84c6c3244f8745ff1c6fa374
Ryan Bigg <me@ryanbigg.com>
2024-03-08 06:55:16 +1100

    Fix remaining replacement effects

--- lib/magic/cards/nine_lives.rb
+++ lib/magic/cards/nine_lives.rb
@@ -6,31 +6,46 @@ module Magic
     end
 
     class NineLives < Enchantment
+      class PreventDamage < ReplacementEffect
+        def applies?(effect)
+          effect.target == receiver.controller
+        end
+
+        def call(effect)
+          Effects::AddCounterToPermanent.new(
+            source: receiver,
+            counter_type: Counters::Incarnation,
+            target: receiver,
+          )
+        end
+      end
+
       class LTB < TriggeredAbility::EnterTheBattlefield
         def perform
           actor.trigger_effect(:lose_game, player: controller)
         end
       end
 
+      class CounterAddedHandler < TriggeredAbility
+        def should_perform?
+          actor.counters.of_type(Counters::Incarnation).count >= 9
+        end
+
+        def call
+          actor.trigger_effect(:exile, target: actor)
+        end
+      end
+
       def replacement_effects
         {
-          Effects::LoseLife => -> (receiver, event) do
-            Effects::AddCounterToPermanent.new(
-              source: receiver,
-              counter_type: Counters::Incarnation,
-              target: receiver,
-            )
-          end
+          Effects::DealDamage => PreventDamage,
+          Effects::DealCombatDamage => PreventDamage,
         }
       end
 
       def event_handlers
         {
-          Events::CounterAddedToPermanent => -> (receiver, event) do
-            if receiver.counters.of_type(Counters::Incarnation).count >= 9
-              trigger_effect(:exile, target: receiver)
-            end
-          end
+          Events::CounterAddedToPermanent => CounterAddedHandler
         }
       end
 
--- lib/magic/cards/pack_leader.rb
+++ lib/magic/cards/pack_leader.rb
@@ -1,23 +1,25 @@
 module Magic
   module Cards
-    PackLeader = Creature("Pack Leader") do
-      creature_type("Dog")
+    class PackLeader < Creature
+      card_name "Pack Leader"
+      creature_type "Dog"
       power 2
       toughness 2
       cost generic: 1, white: 1
-    end
 
-    class PackLeader < Creature
+      class PreventCombatDamage < ReplacementEffect
+        def applies?(effect)
+          receiver.attacking? && effect.target.creature? && effect.target.type?(T::Creatures["Dog"])
+        end
+
+        def call(effect)
+          Effects::Noop.new(source: receiver)
+        end
+      end
+
       def replacement_effects
         {
-          Effects::DealCombatDamage => -> (receiver, event) do
-            # Whenever Pack Leader attacks, prevent all combat damage that would be dealt this turn to Dogs you control.
-            if receiver.attacking? && event.target.creature? && event.target.type?(T::Creatures["Dog"])
-              Effects::Noop.new(source: receiver)
-            else
-              event
-            end
-          end
+          Effects::DealCombatDamage => PreventCombatDamage
         }
       end
 
