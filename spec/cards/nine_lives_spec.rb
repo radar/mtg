de973b6c3b459f44bb7f1028dd86e18d13510223
Ryan Bigg <me@ryanbigg.com>
2026-04-20 09:05:43 +1000

    Implement iterative replacement-effect resolution loop

--- spec/cards/nine_lives_spec.rb
+++ spec/cards/nine_lives_spec.rb
@@ -7,4 +7,24 @@ RSpec.describe Magic::Cards::NineLives do
   it "has hexproof" do
     expect(card.hexproof?).to eq(true)
   end
+
+  context "when replacement creates a new replaceable effect" do
+    let!(:nine_lives) { ResolvePermanent("Nine Lives", owner: p1) }
+    let!(:doubling_season) { ResolvePermanent("Doubling Season", owner: p1) }
+    let!(:attacker) { ResolvePermanent("Wood Elves", owner: p2) }
+
+    it "re-checks replacement effects and doubles the Nine Lives counter" do
+      effect = Magic::Effects::DealDamage.new(
+        source: attacker,
+        target: p1,
+        damage: 1,
+      )
+
+      game.add_effect(effect)
+
+      incarnation_counters = nine_lives.counters.of_type(Magic::Counters::Incarnation).count
+      expect(incarnation_counters).to eq(2)
+      expect(p1.life).to eq(20)
+    end
+  end
 end
