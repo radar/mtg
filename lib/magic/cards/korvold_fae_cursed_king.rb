619eda086defa4ec95cffe9c6de828da6936823a
Ryan Bigg <me@ryanbigg.com>
2026-09-09 11:53:03 +1000

    Complete Korvold sacrifice triggers

--- lib/magic/cards/korvold_fae_cursed_king.rb
+++ lib/magic/cards/korvold_fae_cursed_king.rb
@@ -9,6 +9,30 @@ module Magic
     end
 
     class KorvoldFaeCursedKing < Creature
+      class SacrificeChoice < Magic::Choice::Targeted
+        def choice_amount = 1
+
+        def choices
+          actor.controller.permanents.except(actor)
+        end
+
+        def resolve!(target:)
+          target.sacrifice!
+        end
+      end
+
+      class EntersOrAttacksTrigger < TriggeredAbility
+        def should_perform?
+          (event.is_a?(Events::EnteredTheBattlefield) && event.permanent == actor) ||
+            (event.is_a?(Events::FinalAttackersDeclared) && event.attacks.any? { |attack| attack.attacker == actor })
+        end
+
+        def call
+          choices = SacrificeChoice.new(actor: actor)
+          game.add_choice(choices) if choices.choices.any?
+        end
+      end
+
       class PermanentSacrificedTrigger < TriggeredAbility
         def call
           actor.add_counter("+1/+1")
@@ -16,7 +40,13 @@ module Magic
         end
       end
 
-      def event_handlers = { Events::PermanentSacrificed => PermanentSacrificedTrigger }
+      def event_handlers
+        {
+          Events::EnteredTheBattlefield => EntersOrAttacksTrigger,
+          Events::FinalAttackersDeclared => EntersOrAttacksTrigger,
+          Events::PermanentSacrificed => PermanentSacrificedTrigger,
+        }
+      end
     end
   end
 end
---
+++ spec/cards/korvold_fae_cursed_king_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+RSpec.describe Magic::Cards::KorvoldFaeCursedKing do
+  include_context "two player game"
+
+  it "sacrifices another permanent when it enters and rewards the sacrifice" do
+    target = ResolvePermanent("Forest", owner: p1)
+    ResolvePermanent("Mountain", owner: p1)
+    hand_size = p1.hand.count
+    korvold = ResolvePermanent("Korvold, Fae-Cursed King", owner: p1)
+    game.resolve_choice!(target: target)
+
+    expect(target.card.zone).to be_graveyard
+    expect(korvold.counters.of_type(Magic::Counters::Plus1Plus1).count).to eq(1)
+    expect(p1.hand.count).to eq(hand_size + 1)
+  end
+end
