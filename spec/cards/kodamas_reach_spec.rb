7d344b0b15c6395890e0e32380238c1fa0755b1e
Ryan Bigg <me@ryanbigg.com>
2026-09-09 11:41:36 +1000

    Test Kodama's Reach search choice

---
+++ spec/cards/kodamas_reach_spec.rb
@@ -0,0 +1,18 @@
+require "spec_helper"
+
+RSpec.describe Magic::Cards::KodamasReach do
+  include_context "two player game"
+
+  it "adds a library search choice for up to two basic lands" do
+    spell = Card("Kodama's Reach", owner: p1)
+    p1.hand.add(spell)
+    p1.add_mana(green: 3)
+    p1.cast(card: spell) { _1.pay_mana(green: 1, generic: { green: 2 }) }
+    game.stack.resolve!
+
+    choice = game.choices.last
+    expect(choice).to be_a(Magic::Choice::SearchLibrary)
+    expect(choice.upto).to eq(2)
+    expect(choice.choices).to all(be_basic_land)
+  end
+end
