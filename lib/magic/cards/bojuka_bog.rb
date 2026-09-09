f6a927e1446bdc3568d1b0b3342a7ab19e9776ca
Ryan Bigg <me@ryanbigg.com>
2026-09-09 11:05:17 +1000

    Implement Bojuka Bog

---
+++ lib/magic/cards/bojuka_bog.rb
@@ -0,0 +1,33 @@
+module Magic
+  module Cards
+    BojukaBog = Card("Bojuka Bog") do
+      type "Land"
+      enters_tapped
+
+      enters_the_battlefield do
+        game.add_choice(Choice.new(actor: actor))
+      end
+    end
+
+    class BojukaBog < Card
+      class Choice < Magic::Choice
+        attr_reader :choices
+
+        def initialize(actor:)
+          @choices = actor.game.players
+          super
+        end
+
+        def resolve!(target:)
+          target.graveyard.cards.to_a.each(&:exile!)
+        end
+      end
+
+      class ManaAbility < Magic::TapManaAbility
+        choices :black
+      end
+
+      def activated_abilities = [ManaAbility]
+    end
+  end
+end
