a8323c404011650718d3adfd156dc0c65f04b0ab
Ryan Bigg <me@ryanbigg.com>
2026-09-09 11:05:22 +1000

    Implement Harrow

---
+++ lib/magic/cards/harrow.rb
@@ -0,0 +1,18 @@
+module Magic
+  module Cards
+    class Harrow < Instant
+      card_name "Harrow"
+      cost generic: 2, green: 1
+
+      class Choice < Magic::Choice::SearchLibrary
+        def initialize(actor:)
+          super(actor: actor, upto: 2, to_zone: :battlefield, enters_tapped: false, filter: Filter[:basic_lands])
+        end
+      end
+
+      def resolve!
+        game.add_choice(Choice.new(actor: self))
+      end
+    end
+  end
+end
