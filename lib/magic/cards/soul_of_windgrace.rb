9ac5dc1b7b89aca3384a8fb4b5109a21d76448d2
Ryan Bigg <me@ryanbigg.com>
2026-09-09 11:13:56 +1000

    Implement Soul of Windgrace

---
+++ lib/magic/cards/soul_of_windgrace.rb
@@ -0,0 +1,46 @@
+module Magic
+  module Cards
+    SoulOfWindgrace = Creature("Soul of Windgrace") do
+      legendary_creature_type "Cat Avatar"
+      cost generic: 1, black: 1, red: 1, green: 1
+      power 5
+      toughness 4
+    end
+
+    class SoulOfWindgrace < Creature
+      class LandChoice < Magic::Choice
+        attr_reader :choices
+
+        def initialize(actor:)
+          @choices = actor.game.graveyard_cards.lands
+          super
+        end
+
+        def resolve!(target:)
+          target.resolve!(enters_tapped: true)
+        end
+      end
+
+      class EntersTrigger < TriggeredAbility::EnterTheBattlefield
+        def call
+          choice = LandChoice.new(actor: actor)
+          game.add_choice(choice) if choice.choices.any?
+        end
+      end
+
+      class GainLifeAbility < Magic::ActivatedAbility
+        costs "{G}"
+
+        def resolve!
+          controller.gain_life(3)
+        end
+      end
+
+      def event_handlers
+        { Events::EnteredTheBattlefield => EntersTrigger }
+      end
+
+      def activated_abilities = [GainLifeAbility]
+    end
+  end
+end
