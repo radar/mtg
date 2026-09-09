9caf236ad2c947a737f113d6d5267dca1a5a087a
Ryan Bigg <me@ryanbigg.com>
2026-09-09 11:37:23 +1000

    Implement Worldsoul's Rage

---
+++ lib/magic/cards/worldsouls_rage.rb
@@ -0,0 +1,18 @@
+module Magic
+  module Cards
+    class WorldsoulsRage < Sorcery
+      card_name "Worldsoul's Rage"
+      cost x: 1, red: 1, green: 1
+
+      def target_choices
+        game.any_target
+      end
+
+      def resolve!(target:, value_for_x:)
+        trigger_effect(:deal_damage, target: target, damage: value_for_x)
+        lands = (controller.hand.cards.lands + controller.graveyard.cards.lands).first(value_for_x)
+        lands.each { |land| land.resolve!(enters_tapped: true) }
+      end
+    end
+  end
+end
