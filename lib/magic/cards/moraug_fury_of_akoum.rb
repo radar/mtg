815cb75345f173c652d061ec614b85e642782ae4
Ryan Bigg <me@ryanbigg.com>
2026-09-09 10:37:54 +1000

    Implement Moraug attack trigger

--- lib/magic/cards/moraug_fury_of_akoum.rb
+++ lib/magic/cards/moraug_fury_of_akoum.rb
@@ -6,5 +6,29 @@ module Magic
       power 6
       toughness 6
     end
+
+    class MoraugFuryOfAkoum < Creature
+      class AttackTrigger < TriggeredAbility
+        def should_perform?
+          event.attacks.any? { |attack| attack.attacker == actor }
+        end
+
+        def call
+          attacks_this_turn = game.current_turn.events.count do |event|
+            event.is_a?(Events::AttackDeclared) && event.attack.attacker == actor
+          end
+          actor.trigger_effect(
+            :modify_power_toughness,
+            target: actor,
+            power: attacks_this_turn,
+            until_eot: true,
+          )
+        end
+      end
+
+      def event_handlers
+        { Events::FinalAttackersDeclared => AttackTrigger }
+      end
+    end
   end
 end
--- spec/cards/moraug_fury_of_akoum_spec.rb
+++ spec/cards/moraug_fury_of_akoum_spec.rb
@@ -6,4 +6,15 @@ RSpec.describe Magic::Cards::MoraugFuryOfAkoum do
   it "is a legendary creature" do
     expect(ResolvePermanent("Moraug, Fury Of Akoum", owner: p1)).to be_legendary
   end
+
+  it "gets +1/+0 for each time it attacks this turn" do
+    moraug = ResolvePermanent("Moraug, Fury Of Akoum", owner: p1)
+    skip_to_combat!
+    current_turn.declare_attackers!
+    p1.declare_attacker(attacker: moraug, target: p2)
+    current_turn.attackers_declared!
+    game.tick!
+
+    expect(moraug.power).to eq(7)
+  end
 end
