43d678182f3d52504c3a2914ed2494544fa3283c
Ryan Bigg <me@ryanbigg.com>
2026-09-14 07:45:08 +1000

    Simplify Spelunking

--- lib/magic/cards/spelunking.rb
+++ lib/magic/cards/spelunking.rb
@@ -9,8 +9,8 @@ module Magic
         attr_reader :choices
 
         def initialize(actor:)
-          @choices = actor.controller.hand.lands
           super
+          @choices =hand.lands
         end
 
         def resolve!(target:)
